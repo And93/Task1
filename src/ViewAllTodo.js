a24ebd3b8d18cfc6a286ddb177521853299ca31b
Andrei Dzeichyk <Andrei_Dzeichyk@epam.com>
2018-07-30 09:16:37 +0300

    Upd script of command. Moved number of all todo to the variable

--- src/ViewAllTodo.js
+++ src/ViewAllTodo.js
@@ -8,11 +8,13 @@ class ListOfAllTodo extends ActionWithFile {
 
     viewAllTodo() {
 
-        console.log(`Number of active todo is: ${this.parseredFile().length}`, "\n");
+        const numberOfTodo = this.parseredFile().length;
 
-        if (this.parseredFile().length === 0) {
+        if (numberOfTodo === 0) {
             throw "List of todo is empty. Please create your first todo.";
         }
+
+        console.log(`Number of active todo is: ${numberOfTodo}`, "\n");
 
         this.parseredFile().map(todo => {
             console.log("title:", todo["title"]);
